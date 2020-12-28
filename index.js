f01cd247c04e70b53e36860caa5f5cfee9c69c30
Abir-Hasan-GUB <abirhasan6111@gmail.com>
2020-12-28 17:35:03 +0600

    All order load to admin panel done

--- index.js
+++ index.js
@@ -1,83 +1,111 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const cors = require('cors');
 require('dotenv').config();
-
-
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.19f5u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+app.use(cookieParser());
 
 const port = 5000;
 
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.19f5u.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect(err => {
     const productsCollection = client.db("se-creative-agency").collection("products");
     const reviewsCollection = client.db("se-creative-agency").collection("review");
+    const orderCollection = client.db("se-creative-agency").collection("orders");
 
     // ================ Add a single product to the collection =================
     app.post("/addProdcuct", (req, res) => {
         const product = req.body;
         productsCollection.insertOne(product)
-        .then(result =>{
-            console.log(result.insertedCount)
-            res.send(result.insertedCount)
-        })
+            .then(result => {
+                console.log(result.insertedCount)
+                res.send(result.insertedCount)
+            })
     })
 
     // ================= Load all products =================
-    app.get('/products',(req, res) => {
+    app.get('/products', (req, res) => {
         productsCollection.find({})
-        .toArray((err, documents) => {
-            res.send(documents);
-        })
+            .toArray((err, documents) => {
+                res.send(documents);
+            })
     })
 
     // =============== Load a single product using key =================
-    app.get('/product/:key',(req, res) => {
-        productsCollection.find({key: req.params.key})
-        .toArray((err, documents) => {
-            res.send(documents[0]);//load only one item
-        })
+    app.get('/product/:key', (req, res) => {
+        productsCollection.find({ key: req.params.key })
+            .toArray((err, documents) => {
+                res.send(documents[0]);//load only one item
+            })
     })
 
- // =============== Load a multiple product using key =================
-app.post('/productsByKeys', (req, res) => {
-    const productsKeys = req.body;
-    productsCollection.find({key: {$in: productsKeys}})//load miltiple data using keys(in method)
-    .toArray((err, documents) => {
-        res.send(documents)
+    // =============== Load a multiple product using key =================
+    app.post('/productsByKeys', (req, res) => {
+        const productsKeys = req.body;
+        productsCollection.find({ key: { $in: productsKeys } })//load miltiple data using keys(in method)
+            .toArray((err, documents) => {
+                res.send(documents)
+            })
     })
-})
 
-// ================ Add a review to the collection =================
-app.post("/addReview", (req, res) => {
-    const review = req.body;
-    reviewsCollection.insertOne(review)
-    .then(result =>{
-        res.send(result.insertedCount)
+    // ================ Add a review to the collection =================
+    app.post("/addReview", (req, res) => {
+        const review = req.body;
+        reviewsCollection.insertOne(review)
+            .then(result => {
+                res.send(result)
+            })
     })
-})
 
-// ================= Load all Review Using key in perticuler product =================
-app.get('/allReview/',(req, res) => {
-    reviewsCollection.find({key: req.query.key})
-    .toArray((err, documents) => {
-        console.log(documents)
-        res.send(documents);//load only one item
+    // ================= Load all Review Using key in perticuler product =================
+    app.get('/allReview/', (req, res) => {
+        reviewsCollection.find({ key: req.query.key })
+            .toArray((err, documents) => {
+                res.send(documents);//load item
+            })
     })
-})
 
+    // ================= Load all Review in admin panle= =================
+    app.get('/allReviewInAdmin/', (req, res) => {
+        reviewsCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);//load item
+            })
+    })
 
+    // ================ Add a order to the order collection =================
+    app.post("/addOrder", (req, res) => {
+        const order = req.body;
+        // console.log(order)
+        orderCollection.insertOne(order)
+            .then(result => {
+                res.send(result)
+            })
+    })
 
+    // ================= Load all Order Using email in =================
+    app.get('/clientOrder/', (req, res) => {
+        orderCollection.find({ email: req.query.email })
+            .toArray((err, documents) => {
+                res.send(documents);//load item
+            })
+    })
 
-
+    // ================= Load all Order for admin panel =================
+    app.get('/clientAllOrder', (req, res) => {
+        orderCollection.find({})
+            .toArray((err, documents) => {
+                res.send(documents);//load item
+            })
+    })
 
 
 });
